4e934d0a38848d7c546fd4e3c4d6b8857d8c3de3
nonnax <ironald@gmail.com>
2021-11-25 15:19:06 +0800

    using native ruby threads

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@
 # Id$ nonnax 2021-11-04 21:05:10 +0800
 require 'rubytools/cubadoo'
 require 'csv'
+require 'cgi'
 
 Cuba.class_eval do
   def _layout(&block)
@@ -11,32 +12,89 @@ Cuba.class_eval do
       html do
         head do
           meta('http-equiv': 'refresh', content: '120')
-          link(rel: 'stylesheet', type: "text/css",  href: '/css/style.css')
-          # link(rel: 'stylesheet', type: 'text/css', href: '/media/style.css')
+          # link(rel: 'stylesheet', type: "text/css",  href: '/css/style.css')
+          link(rel: 'stylesheet', type: 'text/css', href: '/css/style001.css')
         end
         body do
-          h1 { 'Layout here' }
+          h1 { 'Layout here' }          
+          p do
+            form(action: '/search', method: 'get') do
+              input( type: 'text', name: 'q' )
+              input( type: 'submit', value: 'search')
+            end
+          end
           div(id: 'content', &block)
         end
       end
     end
   end
+  def location_links(rooms)
+     Scooby.dooby do
+        rooms.map{|r| r[2] }.uniq.each do |loc|
+          a( href: "/search/#{loc}/1"){ loc }
+          span{'&nbsp;'}
+        end
+     end
+  end
+  
+  def render_rooms(rooms, q='*', pg=1 )
+      page=pg.to_i-1
+      offset=200
+      pages=(rooms.size/offset.to_f).ceil
+      
+      render(use_layout: true) do
+        div do
+          p do
+            "room(s): #{page*offset}..#{(page*offset+offset-1)} (#{page+1}/#{pages})"
+          end
+          p do            
+            pages.times do |i|
+              a( href: "/search/#{q}/#{i+1}"){ b(class: 'page'){"#{i+1}"} }
+              span{'&nbsp;&nbsp;'}
+            end
+          end
+          p do
+            rooms.map{|r| r[2] }.uniq.each do |loc|
+              a( href: "/search/#{loc}/1"){ loc }
+              span{'&nbsp;'}
+            end
+          end
+          rooms[page*offset..(page*offset+offset-1)].each do |u|
+            i, user, loc, _, iframe_embbed = u#.split('\\')
+            div(class: 'grid') do
+              div(class: 'center'){a(href: "https://chaturbate.com/#{user}/") { img(src: "/media/#{user}.jpg") }}
+              div(class: 'user') { 
+                p{ user  }
+                p{ loc } 
+              }
+            end            
+          end
+          div(class: 'clearfix'){
+            hr
+          }
+          p do
+            rooms.map{|r| r[2] }.uniq.each do |loc|
+              a( href: "/search/#{loc}/1"){ loc }
+              span{'&nbsp;'}
+            end
+          end
+        end
+      end    
+  end
 end
 
 Cuba.define do
+  rooms = CSV.read('./data.csv')
+
   on get do
     # /media/style.css
     on 'styler', extension('css') do |file|
       render(use_layout: true) { "Filename: #{file}" } #=> "Filename: style"
     end
     # /search?q=barbaz
-    on 'search', param('q') do |query|
-      render(use_layout: true) do
-        div { "You Searched for #{query}" } #=> "Searched for barbaz"
-      end
+    on 'search', param('q') do |q|
+      res.redirect "/search/#{q}/1"
     end
-
-    rooms = CSV.read('./data.csv')
 
     on 'rooms/:page' do |pg|
       page=pg.to_i-1
@@ -67,7 +125,13 @@ Cuba.define do
         end
       end
     end
-    
+
+    on 'search/:loc/:page' do |loc, page|
+       loc_decoded=CGI.unescape(loc)
+       rooms = CSV.read('./data.csv')
+       rooms=rooms.select{|r| (/#{loc_decoded}/i).match(r[2])}
+       render_rooms(rooms,loc,page)
+    end    
     on root do
       res.redirect '/rooms/1'
     end
--- cbph_native.rb
+++ cbph_native.rb
@@ -1,13 +1,118 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 # Id$ nonnax 2021-11-03 00:19:48 +0800
+require 'excon'
+require 'json'
+require 'down'
+require 'fileutils'
+require 'rubytools/array_table'
+require 'rubytools/hash_ext'
+require 'rubytools/thread_ext'
+require 'rubytools/fzf'
+require 'forwardable'
+require 'csv'
+require 'benchmark'
+
+class CBUpdater
+  extend Forwardable
+  def_delegators :@df, :map, :each
+  attr :picklist_ph, :picklist_beauty
+  attr_accessor :df, :th
+
+  def initialize
+    @df = []
+    @th = []
+    @data_file = 'data.txt'
+    File.write(@data_file, '')
+    pickfile = 'picklist_ph'
+    pickfile_beauty = 'picklist_beauty'
+    default_list = %w[murbears_world marymoody cielo69_ candy_temptation_]
+    @picklist_ph = File.exist?(pickfile) ? File.read(pickfile).split("\n").uniq.map(&:strip) : default_list
+    @picklist_beauty = File.exist?(pickfile_beauty) ? File.read(pickfile_beauty).split("\n").uniq.map(&:strip) : default_list
+  end
+
+  def download(image_url)
+    p tmp = Down.download(image_url, max_size: 24 * 1024) # 24kb
+    FileUtils.mv(tmp.path, "./media/#{tmp.original_filename}")
+  end
+
+  def get(offset, display_count = 500)
+    params = {
+      wm: 'LVTEy',
+      client_ip: 'request_ip',
+      limit: display_count,
+      offset: offset,
+      gender: 'f',
+      format: 'json'
+    }
+    url = "https://chaturbate.com/api/public/affiliates/onlinerooms/?#{params.to_query_string}"
+    response = Excon.get(url)
+    JSON.parse(response.body)
+  end
+
+  def populate_df(i)
+    row = []
+    data = get(i * 500)
+
+    return [] if data['results'].empty?
+    p [i, data['results'].size]
+
+    keys = %w[image_url username location age current_show is_hd is_new num_followers iframe_embed]
+    data
+      .to_h['results']
+      .map do |r|
+        row << r.values_at(*keys) if (/hilipp/i).match(r.fetch('location', ''))
+        row << r.values_at(*keys) if r['is_new']
+        row << r.values_at(*keys) if (picklist_ph).include?(r.fetch('username'))
+        row << r.values_at(*keys) if r['age'] && r['age'] < 20
+      end
+
+    @df += row.uniq
+    yield row.uniq
+    # sleep 1
+  end
+
+  def download_media_files(row)
+    row.each do |r|
+      download(r.dup.first)
+    end
+  end
+
+  def save_data_file
+    CSV.open('data.csv', 'w') do |csv|
+      @df.dup.map.with_index do |e, _i|
+        csv << e.values_at(0, 1, 2, 3, -1)
+      end
+    end
+  end
+end
+
+t = []
+worker = CBUpdater.new
+puts Benchmark.measure {
+  11.times do |i|
+    worker.populate_df(i) do |row|
+      row.size.times do |i|
+        t << Thread.new do
+          worker.download(row[i].first)
+        end
+      end
+    end
+  end
+  t.map(&:join)
+
+  worker.save_data_file
+}
+
 # https://chaturbate.com/api/public/affiliates/onlinerooms/?wm=LVTEy&client_ip=request_ip
 # Supported parameters
 # Param 	Choices 	Default 	Comments
 # wm 	LVTEy 	– 	campaign slug (required)
 # client_ip 	an ipv4 or ipv6 ip address or the string request_ip 	–
-# 
+#
 # The ip address used to filter rooms that have blocked users from a country or region (required)
-# 
+#
 # If you are making this request server side on behalf of a client, please use the ip address of the client request so that rooms are correctly filtered, e.g client_ip=230.32.32.6. If you are making this request from the client directly, you should set the parameter value to request_ip to indicate you want to use the requester's ip address, e.g client_ip=request_ip.
 # format 	xml | json | yaml 	json 	The format the data will be returned as
 # limit 	1-500 	100 	The max number of rooms to return in a single API call
@@ -18,16 +123,16 @@
 # region 	asia | europe_russia | northamerica | southamerica | other 	– 	You can filter by multiple regions by including multiple query parameters, e.g region=asia&region=northamerica
 # tag 	– 	– 	You can filter by up to 5 tags by including multiple query parameters, e.g tag=feet&tag=bdsm
 # hd 	true | false 	– 	–
-# response: 
+# response:
 # {
 # "username": "urdreamolivia69",
 # "spoken_languages": "English",
 # "tags": [
-  # "hairy",
-  # "new",
-  # "squirt",
-  # "asian",
-  # "mistress"
+# "hairy",
+# "new",
+# "squirt",
+# "asian",
+# "mistress"
 # ],
 # "current_show": "public",
 # "is_new": false,
@@ -48,107 +153,3 @@
 # "chat_room_url_revshare": "https://chaturbate.com/in/?tour=LQps&campaign=LVTEy&track=default&room=urdreamolivia69",
 # "iframe_embed_revshare": "<iframe src='https://chaturbate.com/in/?tour=9oGW&amp;campaign=LVTEy&amp;track=embed&amp;room=urdreamolivia69&amp;bgcolor=white' height=528 width=850 style='border: none;'></iframe>"
 # },
-require 'excon'
-require 'json'
-require "down"
-require "fileutils"
-require 'rubytools/array_table'
-require 'rubytools/hash_ext'
-require 'rubytools/thread_ext'
-require 'rubytools/fzf'
-require 'forwardable'
-require 'csv'
-require 'benchmark'
-
-class CBUpdater
-  extend Forwardable
-  def_delegators :@df, :map, :each
-  attr :picklist_ph, :picklist_beauty
-  attr_accessor :df, :th
-  def initialize
-    @df=[]
-    @th=[]
-    @data_file='data.txt'
-    File.write(@data_file, '')
-    pickfile='picklist_ph'
-    pickfile_beauty='picklist_beauty'
-    default_list=%w[murbears_world marymoody cielo69_ candy_temptation_]
-    @picklist_ph=File.exists?(pickfile) ? File.read(pickfile).split("\n").uniq.map(&:strip) : default_list
-    @picklist_beauty=File.exists?(pickfile_beauty) ? File.read(pickfile_beauty).split("\n").uniq.map(&:strip) : default_list
-  end
-  
-  def download(image_url)
-    p tmp = Down.download(image_url, max_size: 24*1024) #24kb
-    FileUtils.mv(tmp.path, "./media/#{tmp.original_filename}")
-  end
-  def get(offset, display_count=500)
-    params={
-      wm: 'LVTEy',
-      client_ip: 'request_ip',
-      limit: display_count,
-      offset: offset,
-      gender: 'f',
-      format: 'json'
-    }
-    url="https://chaturbate.com/api/public/affiliates/onlinerooms/?#{params.to_query_string}"
-    response = Excon.get(url)
-    JSON.parse(response.body)
-  end
-
-  def populate_df(i)
-      row = []  
-      data=get(i*500)
-      
-      return [] if data['results'].empty?
-      p [i, data['results'].size]
-      
-      keys=%w[image_url username location age current_show is_hd is_new num_followers iframe_embed]
-      data
-        .to_h
-        .dig('results')
-        .map do |r|
-          row << r.values_at(*keys) if (/hilipp/i).match(r.fetch('location',''))
-          row << r.values_at(*keys) if r['is_new']
-          row << r.values_at(*keys) if (picklist_ph).include?(r.fetch('username'))
-          row << r.values_at(*keys) if (r['age'] && r['age'] < 20 )
-        end
-
-      @df += row.uniq
-      # download_media_files( row )
-      sleep 1
-      yield row.uniq
-  end
-  
-  def download_media_files(row)
-    row.each do |r|
-        download(r.dup.first) 
-    end
-  end
-  def save_data_file
-    CSV.open('data.csv', 'w') do |csv|
-      @df.dup.map.with_index do |e, i|
-        csv << e.values_at(0, 1, 2, 3, -1)
-      end
-    end
-  end
-end
-
-
-t=[]
-worker=CBUpdater.new
-puts Benchmark.measure {
-
-  11.times do | i |
-      worker.populate_df(i) do |row|
-        row.size.times do |i|
-          t<<Thread.new do
-            worker.download(row[i].first)
-          end
-        end
-     end
-  end
-  t.map(&:join)
-  
-  worker.save_data_file
-  
-}
