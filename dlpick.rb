270417889c57749afc9d19e1325ba9475629b2b8
nonnax <iroanld@gmail.com>
2022-02-09 12:31:28 +0800

    update: cb.conf

--- dlpick.rb
+++ dlpick.rb
@@ -3,23 +3,37 @@
 require 'csv'
 require './downloader'
 require 'rubytools/console_ext'
+require 'rubytools/thread_ext'
 
 NAME_COL=2
 URL_COL=1
 
 data=CSV.parse(File.read('data.csv'))
 picks=CSV.parse(File.read('picklist')).flatten
-
-online=data
-  .select{|e| picks.include?e[NAME_COL] }
-
-
-IO::Screen.quiet_draw do
-  online
+ 
+def get_data(data, picks)
+  # Monitor.new.synchronize do
+    data
+    .select{|e| picks.include?e[NAME_COL] }
     .map{|e| e[URL_COL]}
-    .sort
-    .each_with_index{|e, i| 
-        Downloader.download(e, 'live')
-        print "%<size>d%%\r" % {size: (i/online.size.to_f)*100}
-      }
+  # end
 end
+
+def download(data, picks)
+  t=[]
+    online=get_data(data, picks)
+    online
+      .each_with_index do |e, i| 
+        t<<Thread.new(e, i)  do |e, i |
+            Downloader.download(e, 'live')
+            # print "%<size>d%%\r" % {size: (i/online.size.to_f)*100}
+            title = "% <i>s of %{size}" % {i:, size: online.size}
+            puts title.rjust(10)
+            sleep 0.5
+          end
+    end
+  t.map(&:join)
+end
+
+download(data, picks)
+
